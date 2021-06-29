4ab8045357ce2356af937ed12b188c43d844384f
Daniel O <danielolvera2188@gmail.com>
2021-06-28 23:24:30 -0600

    generate html file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output');
+});
